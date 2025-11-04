fda8ddb3138c9d17cd56117b27c1d4e3eabd8122
re220519 <kreshma.k54@gmail.com>
2025-11-04 14:32:57 +0400

    adding task2

---
+++ taskone.js
@@ -0,0 +1,12 @@
+//Declare const day = 3.
+//Use a switch statement to print the day of the week based on the number
+//Try changing day to another value before the switch and observe what happens.
+switch(day){
+   case1 :"sunday",
+   console.log("sunday");
+   case2:"monday",
+   console.log("monday");
+   case3:"tuesday",
+   console.log("tuesday");
+   break;
+}
